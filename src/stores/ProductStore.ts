d5235d3fbc136d7fbfe0cc1ec1aa73042ea61a1a
ZephyrsWhisper <hardyza@mail.gvsu.edu>
2024-04-09 14:34:53 -0400

    Updated ProductsStore
    Implemented require functions.

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -3,5 +3,21 @@ import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
 
 export const useProductStore = defineStore("ProductStore", {
-  // your answer
+  
+  state: () => ({products: <ProductDoc[]>[]}),
+  getters:{
+
+  },
+  actions:{
+    init(){
+      this.products = initProducts;
+    },
+     filterByCategory(category: String){
+      this.products.filter(x => x.data.category == category);
+    },
+    filterByRating(minRating: number){
+      this.products.filter(x => x.data.rating >= minRating)
+    }
+  }
+
 });
