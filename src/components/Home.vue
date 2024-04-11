51230022441d2b84d9e418029b0dd95a595a6856
kylewscott <kylewscott512@gmail.com>
2024-04-11 12:54:59 -0500

    StoreItem and Home working

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,22 @@
+Cricket — Today at 11:39 AM
 <template>
-  <!-- your answer -->
+<v-container> 
+  <v-row> 
+    <v-col v-for="product in products" :key="product.id" cols="12" sm="6" md="4">
+        <store-item :product="product" :style="{height: '380px', width: '340px'}"></store-item>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <script lang="ts" setup>
 // your answer
+
+import StoreItem from './StoreItem.vue';
+import { useProductStore } from '../stores/ProductStore';
+
+const ProdStore = useProductStore();
+ProdStore.init();
+const products = ProdStore.products;
+
 </script>
--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,7 +1,18 @@
 <template>
-  <!-- your answer -->
+  <v-card>
+    <v-card-title>{{  product.data.name }}</v-card-title>
+    <v-img height="150" :src="product.data.image"  contain></v-img>
+    <v-card-text>{{ product.data.description }}</v-card-text> 
+    <v-card-subtitle>Price: ${{product.data.price}}</v-card-subtitle>
+    <v-card-subtitle>Rating: {{product.data.rating}} / 5</v-card-subtitle>
+    <v-card-subtitle>Stock: {{product.data.stock}}</v-card-subtitle>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
-// your answer
+import { ProductDoc } from '../types/product.ts';
+import { defineProps } from 'vue';
+defineProps<{
+  product: ProductDoc;
+}>();
 </script>
